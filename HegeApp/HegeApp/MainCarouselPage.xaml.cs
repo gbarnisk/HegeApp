00763a9a769282547f7f253b8e7ad2487fff3323
williammilch <wmilch@macalester.edu>
2018-10-13 21:45:44 -0500

    Updated images on carousel screen

--- HegeApp/HegeApp/MainCarouselPage.xaml.cs
+++ HegeApp/HegeApp/MainCarouselPage.xaml.cs
@@ -11,7 +11,7 @@ namespace HegeApp
         {
             InitializeComponent();
             MyImage.Source = ImageSource.FromFile("Life_on_the_hege.png");
-            MyImage2.Source = ImageSource.FromFile("The_Hege_gets_a_job.png");
+           MyImage2.Source = ImageSource.FromFile("The_Hege_gets_a_job.png");
             MyImage3.Source = ImageSource.FromFile("The_Last_Minute_Issue.png");
 
         }
