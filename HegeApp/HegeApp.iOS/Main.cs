38bb5bb37e20eaec303b298b463b533397d11db9
williammilch <wmilch@macalester.edu>
2018-11-14 10:19:16 -0600

    Removed CloudRail... a second time

--- HegeApp/HegeApp.iOS/Main.cs
+++ HegeApp/HegeApp.iOS/Main.cs
@@ -1,5 +1,4 @@
 ﻿using UIKit;
-using CloudRailSI;
 
 namespace HegeApp.iOS
 {
@@ -8,28 +7,9 @@ namespace HegeApp.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            CRCloudRail.AppKey = "5bd9c18b16d0d558c940a685";
-            System.Console.WriteLine("app key");
-            CRDropbox service = new CRDropbox("EKFhBHnV6xAAAAAAAAAAHtnOPL3NwBpuCbOhLyWkcLejzM-A-B3ZHtWusqoKKp9X", "o7iki0jg52f4pnn");
-            System.Console.WriteLine("db key 1");
-            service.UseAdvancedAuthentication();
-            System.Console.WriteLine("advanced authentication");
-            service.Login();
-            System.Console.WriteLine("login");
-             //if you want to use a different Application Delegate class from "AppDelegate"
-             //you can specify it here.
-
-            //CRGoogleDrive shilad = new CRGoogleDrive("1016822824176-6ieibidk36rh7oucvalek1o0gqfv7bbs.apps.googleusercontent.com", "", "", "state");
-            //System.Console.WriteLine("db key");
-            //shilad.UseAdvancedAuthentication();
-            //System.Console.WriteLine("advanced authentication");
-            //shilad.Login();
-            //System.Console.WriteLine("login");
-            //UIApplication.Main(args, null, "AppDelegate");
-
-
-            CRCloudMetaData[] result = Foundation.NSArray.FromArray<CRCloudMetaData>(service.ChildrenOfFolderWithPath(
-                "/Apps/HegeApp"));
+            // if you want to use a different Application Delegate class from "AppDelegate"
+            // you can specify it here.
+            UIApplication.Main(args, null, "AppDelegate");
 
         }
     }
