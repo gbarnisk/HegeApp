42f580ed2a305913c8466e5c49c847884fb1304c
gbarnisk <31669608+gbarnisk@users.noreply.github.com>
2018-12-14 12:27:44 -0600

    Added app icon on Android.

--- HegeApp/HegeApp.Android/MainActivity.cs
+++ HegeApp/HegeApp.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using System.Linq;
 
 namespace HegeApp.Droid
 {
-    [Activity(Label = "HegeApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "HegeApp", Icon = "@drawable/Icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
